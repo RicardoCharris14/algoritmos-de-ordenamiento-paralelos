fd9a19d7b58918d7cfe8969f44c6d99c0641ec40
Nikolexion <nfalc2003@gmail.com>
2026-04-26 12:36:20 -0400

    fix: agregado firstprivate a variables en la version paralela de merge sort

--- ms.cpp
+++ ms.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <cstdlib>
+#include <random>
 #include "merge.h"
 
 // Umbral de corte para la recursión en paralelo
@@ -20,23 +21,22 @@ void sequentialMergeSort(std::vector<int>& arr, int left, int right) {
 }
 
 void parallelMergeSort(std::vector<int>& arr, int left, int right) {
+    if (left >= right) return;
 
     if (right - left < UMBRAL) {
         sequentialMergeSort(arr, left, right);
         return;
     }
 
-    if (left >= right) return;
-    
     int mid = left + (right - left) / 2;
-    
-    #pragma omp task shared(arr)
+
+    #pragma omp task shared(arr) firstprivate(left, mid)
     parallelMergeSort(arr, left, mid);
-    
-    #pragma omp task shared(arr)
+
+    #pragma omp task shared(arr) firstprivate(mid, right)
     parallelMergeSort(arr, mid + 1, right);
-    
+
     #pragma omp taskwait
-    
+
     merge(arr, left, mid, right);
 }
